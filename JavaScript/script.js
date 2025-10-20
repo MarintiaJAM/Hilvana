fa427a794a87843797af9e26c84fdad300ba8690
Jaime1945 <jaimejuliancastillo23@cbtis52.edu.mx>
2025-10-20 12:47:53 -0600

    Menu despegable añanido, con un problemita

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,3 +12,23 @@ const botonesFavorito = document.querySelectorAll('.favorito');
     });
   });
 
+  //menu despegable
+    const hamburgerBtn = document.getElementById('hamburgerBtn');
+    const sideMenu = document.getElementById('sideMenu');
+    const overlay = document.getElementById('overlay');
+
+    function toggleSideMenu() {
+        const isOpen = sideMenu.style.left === '0px';
+
+        if (isOpen) {
+            sideMenu.style.left = '-250px';
+            overlay.style.display = 'none';
+            document.body.classList.remove('menu-open');
+        } else {
+            sideMenu.style.left = '0px';
+            overlay.style.display = 'block';
+            document.body.classList.add('menu-open');
+        }
+    }
+
+    hamburgerBtn.addEventListener('click', toggleSideMenu);
